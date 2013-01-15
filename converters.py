eaf5b4bd299c67dd4f5c8e26d6fd0313816493a4
Eelke Hermens <eelke@Eelkes-MacBook-Air.local>
2013-01-15 21:44:23 +0100

    file export extension is now csv

--- converters.py
+++ converters.py
@@ -3,7 +3,7 @@ import base64
 import hashlib
 
 def list_to_tab(alist):
-    fname = '%s.tab' % base64.urlsafe_b64encode(hashlib.md5(str(alist)).digest())[:-2] 
+    fname = '%s.csv' % base64.urlsafe_b64encode(hashlib.md5(str(alist)).digest())[:-2] 
     f = codecs.open('results/%s' % fname, 'w', encoding='utf-8')
     f.write('%s\n' % '\t'.join([unicode(v) for v in alist[0].keys()]))  
     for d in alist:
