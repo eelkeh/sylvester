5ee58f3b37e6c026afd005fdc40fa63bfc6d2e05
Eelke Hermens <eelkehermens@gmail.com>
2013-11-28 10:28:35 +0100

    remove dupe files

--- example.py
+++
@@ -1,8 +0,0 @@
-import sylvester
-import json
-	
-pool = sylvester.create_client_pool_from_json()
-screen_name = "Twitter"
-for tweets in pool.get_timeline(screen_name):
-	with open('tweets.json', 'a') as outfile:
-		json.dump(tweets, outfile)
